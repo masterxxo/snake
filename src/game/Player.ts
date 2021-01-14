0fa61c9376cb23c17e044452170cc60fc24ab5ff
Mateusz <kontakt@matrog.pl>
2021-01-14 22:23:37 +0100

    Setting up player class

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -1,9 +1,18 @@
 export default class Player {
     /** @type {Phaser.Scene} */
     scene: Phaser.Scene;
+    private lastMoveTime: Number;
+    private moveInterval: Number = 100;
+    private tileSize: Number = 16;
+    /** @type {Phaser.Math.Vector2} */
+    private direction: Phaser.Math.Vector2;
+    private body: Array<Phaser.GameObjects.Rectangle>
 
     constructor(scene) {
         this.scene = scene;
+        this.lastMoveTime = 0;
+        this.direction = Phaser.Math.Vector2.LEFT;
+        this.body = [];
     }
 
 }
